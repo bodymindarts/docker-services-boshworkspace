6f1fd0424783d08c134e3bf6096d3c09231a8a8f
Justin Carter <jcarter@starkandwayne.com>
2015-12-10 08:46:31 -0500

    Filter stacktrace from rspec output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,19 @@
+module DirHeplers
+  def spec_dir
+    File.expand_path('../', __FILE__)
+  end
+
+  def result_file(name)
+    File.join(spec_dir, 'results', name)
+  end
+
+  def stub_file(name)
+    File.join(spec_dir, 'stubs', name)
+  end
+end
+
 RSpec.configure do |config|
+  config.extend DirHeplers
+  config.backtrace_exclusion_patterns = [ /gems/ ]
   config.color = true
-end
-
-def spec_dir
-  File.expand_path('../', __FILE__)
-end
-
-def result_file(name)
-  File.join(spec_dir, 'results', name)
-end
-
-def stub_file(name)
-  File.join(spec_dir, 'stubs', name)
 end
